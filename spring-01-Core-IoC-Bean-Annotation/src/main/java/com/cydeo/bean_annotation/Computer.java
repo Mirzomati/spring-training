869aea37964da931be3462823c6c38240807f05f
Muzaffarzoda <muzaffarzoda@gmail.com>
2023-02-25 18:57:18 -0500

    how to create and use spring container

---
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/Computer.java
@@ -0,0 +1,13 @@
+package com.cydeo.bean_annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Computer {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = new AnnotationConfigApplicationContext();
+
+    }
+}
