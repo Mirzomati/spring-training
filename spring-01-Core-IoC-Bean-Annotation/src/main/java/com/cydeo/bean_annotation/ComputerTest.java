5daa643b13058addd6b1f5a8d48518025b873242
Muzaffarzoda <muzaffarzoda@gmail.com>
2023-02-25 19:13:45 -0500

    Change class

--- spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
@@ -12,4 +12,10 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+public class ComputerTest{
+    public static void main(String[] args) {
 
+
+        ApplicationContext container = new AnnotationConfigApplicationContext();
+    }
+}
